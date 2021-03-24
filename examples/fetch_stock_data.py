a19b4cd99fa915497d6942ac0c061207b86de878
joxer <26515+joxer@users.noreply.github.com>
2021-03-24 13:23:52 +0000

    first version of stock_utilities

---
+++ examples/fetch_stock_data.py
@@ -0,0 +1,18 @@
+import datetime
+
+import stock_utilities
+
+data = stock_utilities.stock_data.StockData(
+    "SPY", stock_utilities.proxy.YFinanceProvider
+)
+print(data.get_last_price())
+data = stock_utilities.stock_data.StockData(
+    "SPY", stock_utilities.proxy.YFinanceProvider
+)
+history = data.get_stock_price_history(
+    interval=datetime.timedelta(days=1), period=datetime.timedelta(days=5)
+)
+assert len(history), 5
+
+history_option = data.get_next_friday_option_chain()
+print(history_option)
